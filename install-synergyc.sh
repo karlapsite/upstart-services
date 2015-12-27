f53c02b07a86408345d5543cac369478172b64cc
Karl Apsite <KarlApsite@gmail.com>
2015-12-27 11:41:24 -0500

    Made room for the server config
    I'm going to have to rename this repo eventually

--- install-synergyc.sh
+++ install-synergyc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+SYNERGYC='/etc/init/synergyc.conf'
+
 if [[ ! -f /etc/default/synergy ]] ; then
-cat <<'EOF' > /tmp/synergy
+cat <<'EOF' > /tmp/synergyc
 # The server address is of the form: [<hostname>][:<port>].  The hostname
 # must be the address or hostname of the server.  The port overrides the
 # default port, 24800.
@@ -8,39 +10,41 @@ SYNERGY_SERVER=127.0.0.1
 SYNERGY_PORT=24800
 EOF
 
-    sudo mv /tmp/synergy /etc/default/synergy
-    sudo "${EDITOR:-vi}" /etc/default/synergy
+    sudo mv /tmp/synergyc/etc/default/synergyc
+    sudo "${EDITOR:-vi}" /etc/default/synergyc
 fi
 
 # Check to see if the configuration already exists
-if [[ -e /etc/init/synergy.conf ]] ; then
+if [[ -e "$SYNERGYC" ]] ; then
     echo "The 'synergy.conf' service config is already installed."
     read -p "Overwrite the service configuration? [Y/n] "
     if [[ $REPLY =~ ^N$|^n$ ]] ; then
         exit 1
     else
-        sudo rm -f /etc/init/synergy.conf
+        sudo rm -f "$SYNERGYC"
     fi
 fi
 
 # Create a symbolic link
 # sudo ln -s `pwd`/synergy.conf /etc/init/synergy.conf
 # Actually... just copy it because that's how I set up other PC's with split /home and / partitions
-sudo cp synergy.conf /etc/init/synergy.conf
+sudo cp synergyc.conf            "$SYNERGYC"
 
 # Reload upstart configuration, so it knows about the services we added
 sudo initctl reload-configuration
 
 # Check to see if synergy is already running
-if P=$(pgrep synergy)
+if P=$(pgrep synergyc)
 then
-    echo "A service named synergy is already running"
-    read -p "Restart synergy? [Y/n] "
-    if [[ $REPLY =~ ^N$|^n$ ]] ; then
-        exit 1
+    echo "A service named synergyc is already running"
+    read -p "Restart synergy? [y/N] "
+    if [[ $REPLY =~ ^Y$|^y$ ]] ; then
+        sudo service synergyc stop
+        # Start the new service
+        sudo service synergyc start
     else
-        sudo service synergy stop
+        exit 1
     fi
 fi
-# Start the new service
-sudo service synergy start
+
+exit 0
