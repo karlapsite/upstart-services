bc8a0a7dd320fd2e827c5c814823f3938b14f1bf
CptSpaceToaster <CptSpaceToaster@gmail.com>
2015-12-27 12:29:58 -0500

    Add an uninstall script
    Maybe I should combine these two?

---
+++ uninstall-synergyc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+BIN="synergyc"
+TMP="/tmp/$BIN"
+ETC_CONF="/etc/default/$BIN.conf"
+UPSTART_CONF="/etc/init/$BIN.conf"
+
+read -p "Uninstall $BIN configuration? [y/N] "
+if [[ $REPLY =~ ^Y$|^y$ ]] ; then
+    # stop the service
+    sudo service $BIN stop
+
+    # clean up assocated configurations
+    rm -rf "$TMP"
+    sudo rm -rf "$ETC_CONF"
+    sudo rm -rf "$UPSTART_CONF"
+
+    read -p "Remove logs? [y/N] "
+    if [[ $REPLY =~ ^Y$|^y$ ]] ; then
+        sudo rm -rf "/var/log/upstart/$BIN.log"*
+    fi
+
+    # Reload upstart configuration, so it knows about the service we changed
+    sudo initctl reload-configuration
+else
+    exit 1
+fi
+
+exit 0
